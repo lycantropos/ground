611c9105f84f1f3e27e9070b7821cadec39b5e25
Azat Ibrakov <azatibrakov@gmail.com>
2020-12-17 13:30:12 +0300

    Complete 'functions.set_context' function

--- ground/functions.py
+++ ground/functions.py
@@ -110,6 +110,10 @@ def get_context() -> Context:
 
 
 def set_context(context: Context) -> None:
+    assert isinstance(context, Context), ('expected "{expected}" instance, '
+                                          'but got "{actual}".'
+                                          .format(expected=Context,
+                                                  actual=context))
     _context_factory.set(context)
 
 
