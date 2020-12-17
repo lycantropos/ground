205e9f7323e563ec8a5cbd33b159b34219c38c90
Azat Ibrakov <azatibrakov@gmail.com>
2020-12-17 13:30:02 +0300

    Complete 'coordinates.set_context' function

--- ground/coordinates.py
+++ ground/coordinates.py
@@ -80,6 +80,10 @@ def get_context() -> Context:
 
 
 def set_context(context: Context) -> None:
+    assert isinstance(context, Context), ('expected "{expected}" instance, '
+                                          'but got "{actual}".'
+                                          .format(expected=Context,
+                                                  actual=context))
     _context.set(context)
 
 
