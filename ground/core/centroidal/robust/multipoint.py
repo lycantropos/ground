de2edb77b75dbb3e918889d9ed33bafb59da6dbc
Azat Ibrakov <azatibrakov@gmail.com>
2020-12-24 20:03:23 +0300

    Complete 'Context.multipoint_centroid' method

--- ground/core/centroidal/robust/multipoint.py
+++ ground/core/centroidal/robust/multipoint.py
@@ -1,5 +1,6 @@
 from fractions import Fraction
-from typing import Type
+from typing import (Callable,
+                    Type)
 
 from ground.core.shewchuk import add_to_expansion
 from ground.hints import (Multipoint,
@@ -7,11 +8,13 @@ from ground.hints import (Multipoint,
 
 
 def centroid(point_cls: Type[Point],
-             multipoint: Multipoint) -> Point:
+             multipoint: Multipoint,
+             inverse: Callable[[int], Fraction] = Fraction(1).__truediv__
+             ) -> Point:
     result_x = result_y = (0,)
     for point in multipoint.points:
         result_x = add_to_expansion(result_x, point.x)
         result_y = add_to_expansion(result_y, point.y)
-    inverted_points_count = Fraction(1, len(multipoint.points))
+    inverted_points_count = inverse(len(multipoint.points))
     return point_cls(result_x[-1] * inverted_points_count,
                      result_y[-1] * inverted_points_count)
