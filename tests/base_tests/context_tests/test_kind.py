80ed5a86705cdf688206cfd8d275e62cc274ad6d
Azat Ibrakov <azatibrakov@gmail.com>
2020-12-24 14:06:32 +0300

    Add test for 'Context.kind' method

---
+++ tests/base_tests/context_tests/test_kind.py
@@ -0,0 +1,39 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from ground.base import (Context,
+                         Kind)
+from ground.hints import Point
+from . import strategies
+
+
+@given(strategies.contexts, strategies.points_triplets)
+def test_basic(context: Context,
+               points_triplet: Tuple[Point, Point, Point]) -> None:
+    vertex, first_ray_point, second_ray_point = points_triplet
+
+    result = context.kind(vertex, first_ray_point, second_ray_point)
+
+    assert isinstance(result, Kind)
+
+
+@given(strategies.contexts, strategies.points_pairs)
+def test_same_endpoints(context: Context,
+                        points_pair: Tuple[Point, Point]) -> None:
+    start, end = points_pair
+
+    assert context.kind(end, start, start) is (Kind.RIGHT
+                                               if start == end
+                                               else Kind.ACUTE)
+
+
+@given(strategies.contexts, strategies.points_triplets)
+def test_endpoints_permutations(context: Context,
+                                points_triplet: Tuple[Point, Point, Point]
+                                ) -> None:
+    vertex, first_ray_point, second_ray_point = points_triplet
+
+    result = context.kind(vertex, first_ray_point, second_ray_point)
+
+    assert result is context.kind(vertex, second_ray_point, first_ray_point)
