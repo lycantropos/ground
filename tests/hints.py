c01e6f1fb13a1f0eadb82f24530d73b648eb0c9f
Azat Ibrakov <azatibrakov@gmail.com>
2020-12-26 11:52:53 +0300

    Complete tests type hints

--- tests/hints.py
+++ tests/hints.py
@@ -4,15 +4,11 @@ from typing import (Sequence,
 
 from hypothesis.strategies import SearchStrategy
 
-from ground.core.hints import QuaternaryPointFunction
-from ground.hints import (Coordinate,
-                          Point)
+from ground.hints import Point
 
 Domain = TypeVar('Domain')
 Range = TypeVar('Range')
 Strategy = SearchStrategy
-CrossProducer = DotProducer = IncircleDeterminer = (
-    QuaternaryPointFunction[Coordinate])
 PointsPair = Tuple[Point, Point]
 PointsQuadruplet = Tuple[Point, Point, Point, Point]
 PointsTriplet = Tuple[Point, Point, Point]
