26eaa74f039305401a6b7c31ba737090fd9d8872
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-14 20:58:27 +0300

    Complete 'core.geometries' module

--- ground/core/geometries.py
+++ ground/core/geometries.py
@@ -13,7 +13,13 @@ class Point:
     def __init__(self, x: hints.Coordinate, y: hints.Coordinate) -> None:
         self._x, self._y = x, y
 
-    __repr__ = generate_repr(__init__)
+    @property
+    def x(self) -> hints.Coordinate:
+        return self._x
+
+    @property
+    def y(self) -> hints.Coordinate:
+        return self._y
 
     def __eq__(self, other: 'Point') -> bool:
         return (self.x == other.x and self.y == other.y
@@ -33,13 +39,7 @@ class Point:
                 if isinstance(other, Point)
                 else NotImplemented)
 
-    @property
-    def x(self) -> hints.Coordinate:
-        return self._x
-
-    @property
-    def y(self) -> hints.Coordinate:
-        return self._y
+    __repr__ = generate_repr(__init__)
 
 
 class Multipoint:
@@ -48,14 +48,14 @@ class Multipoint:
     def __init__(self, points: Sequence[hints.Point]) -> None:
         self._points = points
 
-    __repr__ = generate_repr(__init__)
+    @property
+    def points(self) -> Sequence[hints.Point]:
+        return self._points
 
     def __eq__(self, other: 'Multipoint') -> bool:
         return are_sequences_equivalent(self.points, other.points)
 
-    @property
-    def points(self) -> Sequence[hints.Point]:
-        return self._points
+    __repr__ = generate_repr(__init__)
 
 
 class Segment:
@@ -63,14 +63,6 @@ class Segment:
 
     def __init__(self, start: hints.Point, end: hints.Point) -> None:
         self._start, self._end = start, end
-
-    __repr__ = generate_repr(__init__)
-
-    def __eq__(self, other: 'Segment') -> bool:
-        return (self.start == other.start and self.end == other.end
-                or self.start == other.end and self.end == other.start
-                if isinstance(other, Segment)
-                else NotImplemented)
 
     @property
     def start(self) -> hints.Point:
@@ -80,6 +72,14 @@ class Segment:
     def end(self) -> hints.Point:
         return self._end
 
+    def __eq__(self, other: 'Segment') -> bool:
+        return (self.start == other.start and self.end == other.end
+                or self.start == other.end and self.end == other.start
+                if isinstance(other, Segment)
+                else NotImplemented)
+
+    __repr__ = generate_repr(__init__)
+
 
 class Multisegment:
     __slots__ = '_segments',
@@ -87,14 +87,14 @@ class Multisegment:
     def __init__(self, segments: Sequence[hints.Segment]) -> None:
         self._segments = segments
 
-    __repr__ = generate_repr(__init__)
+    @property
+    def segments(self) -> Sequence[hints.Segment]:
+        return self._segments
 
     def __eq__(self, other: 'Multisegment') -> bool:
         return are_sequences_equivalent(self.segments, other.segments)
 
-    @property
-    def segments(self) -> Sequence[hints.Segment]:
-        return self._segments
+    __repr__ = generate_repr(__init__)
 
 
 class Contour:
@@ -103,14 +103,14 @@ class Contour:
     def __init__(self, vertices: Sequence[hints.Polygon]) -> None:
         self._vertices = vertices
 
-    __repr__ = generate_repr(__init__)
+    @property
+    def vertices(self) -> Sequence[hints.Polygon]:
+        return self._vertices
 
     def __eq__(self, other: 'Contour') -> bool:
         return are_sequences_equivalent(self.vertices, other.vertices)
 
-    @property
-    def vertices(self) -> Sequence[hints.Polygon]:
-        return self._vertices
+    __repr__ = generate_repr(__init__)
 
 
 class Box:
@@ -123,14 +123,6 @@ class Box:
                  max_y: hints.Coordinate) -> None:
         self._min_x, self._max_x, self._min_y, self._max_y = (min_x, max_x,
                                                               min_y, max_y)
-
-    __repr__ = generate_repr(__init__)
-
-    def __eq__(self, other: 'Box') -> bool:
-        return (self.min_x == other.min_x and self.max_x == other.max_x
-                and self.min_y == other.min_y and self.max_y == other.max_y
-                if isinstance(other, Box)
-                else NotImplemented)
 
     @property
     def max_x(self) -> hints.Coordinate:
@@ -148,6 +140,14 @@ class Box:
     def min_y(self) -> hints.Coordinate:
         return self._min_y
 
+    def __eq__(self, other: 'Box') -> bool:
+        return (self.min_x == other.min_x and self.max_x == other.max_x
+                and self.min_y == other.min_y and self.max_y == other.max_y
+                if isinstance(other, Box)
+                else NotImplemented)
+
+    __repr__ = generate_repr(__init__)
+
 
 class Polygon:
     __slots__ = '_border', '_holes'
@@ -155,12 +155,6 @@ class Polygon:
     def __init__(self, border: hints.Contour, holes: Sequence[hints.Contour]
                  ) -> None:
         self._border, self._holes = border, holes
-
-    __repr__ = generate_repr(__init__)
-
-    def __eq__(self, other: 'Polygon') -> bool:
-        return (self.border == other.border
-                and are_sequences_equivalent(self.holes, other.holes))
 
     @property
     def border(self) -> hints.Contour:
@@ -170,6 +164,12 @@ class Polygon:
     def holes(self) -> Sequence[hints.Contour]:
         return self._holes
 
+    def __eq__(self, other: 'Polygon') -> bool:
+        return (self.border == other.border
+                and are_sequences_equivalent(self.holes, other.holes))
+
+    __repr__ = generate_repr(__init__)
+
 
 class Multipolygon:
     __slots__ = '_polygons',
@@ -177,14 +177,14 @@ class Multipolygon:
     def __init__(self, polygons: Sequence[hints.Polygon]) -> None:
         self._polygons = polygons
 
-    __repr__ = generate_repr(__init__)
+    @property
+    def polygons(self) -> Sequence[hints.Polygon]:
+        return self._polygons
 
     def __eq__(self, other: 'Multipolygon') -> bool:
         return are_sequences_equivalent(self.polygons, other.polygons)
 
-    @property
-    def polygons(self) -> Sequence[hints.Polygon]:
-        return self._polygons
+    __repr__ = generate_repr(__init__)
 
 
 _T = TypeVar('_T')
