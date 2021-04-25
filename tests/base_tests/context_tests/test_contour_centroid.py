7319b691b49a3dc35e25c4ae50cd1ee971a02653
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-25 21:36:45 +0300

    Complete test for 'Context.contour_centroid' method

--- tests/base_tests/context_tests/test_contour_centroid.py
+++ tests/base_tests/context_tests/test_contour_centroid.py
@@ -6,6 +6,7 @@ from hypothesis import given
 from ground.base import Context
 from tests.utils import (Point,
                          is_point,
+                         reverse_sequence,
                          rotate_sequence)
 from . import strategies
 
@@ -17,6 +18,16 @@ def test_basic(context_with_vertices: Tuple[Context, Sequence[Point]]) -> None:
     result = context.contour_centroid(vertices)
 
     assert is_point(result)
+
+
+@given(strategies.contexts_with_rational_vertices)
+def test_reversals(context_with_vertices: Tuple[Context, Sequence[Point]]
+                   ) -> None:
+    context, vertices = context_with_vertices
+
+    result = context.contour_centroid(vertices)
+
+    assert result == context.contour_centroid(reverse_sequence(vertices))
 
 
 @given(strategies.contexts_with_rational_vertices, strategies.indices)
