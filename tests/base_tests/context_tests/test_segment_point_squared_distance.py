879a802cf693b65984132e36bdbbdd06948a7368
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-29 17:43:34 +0300

    Complete test for 'Context.segment_point_squared_distance' method

--- tests/base_tests/context_tests/test_segment_point_squared_distance.py
+++ tests/base_tests/context_tests/test_segment_point_squared_distance.py
@@ -3,42 +3,44 @@ from typing import Tuple
 from hypothesis import given
 
 from ground.base import Context
-from ground.hints import Point
-from tests.hints import PointsPair
-from tests.utils import is_coordinate
+from ground.hints import (Point,
+                          Segment)
+from tests.utils import (is_coordinate,
+                         reverse_point_coordinates,
+                         reverse_segment,
+                         reverse_segment_coordinates)
 from . import strategies
 
 
-@given(strategies.contexts_with_segments_endpoints_and_points)
-def test_basic(context_with_segment_endpoints_and_point
-               : Tuple[Context, PointsPair, Point]) -> None:
-    (context, segment_endpoints,
-     point) = context_with_segment_endpoints_and_point
-    start, end = segment_endpoints
+@given(strategies.contexts_with_segments_and_points)
+def test_basic(context_with_segment_and_point: Tuple[Context, Segment, Point]
+               ) -> None:
+    context, segment, point = context_with_segment_and_point
 
-    result = context.segment_point_squared_distance(start, end, point)
+    result = context.segment_point_squared_distance(segment, point)
 
     assert is_coordinate(result)
 
 
-@given(strategies.contexts_with_rational_segments_endpoints_and_points)
-def test_endpoints_symmetry(context_with_segment_endpoints_and_point
-                            : Tuple[Context, PointsPair, Point]) -> None:
-    (context, segment_endpoints,
-     point) = context_with_segment_endpoints_and_point
-    start, end = segment_endpoints
+@given(strategies.contexts_with_rational_segments_and_points)
+def test_reversals(context_with_segment_and_point
+                   : Tuple[Context, Segment, Point]) -> None:
+    context, segment, point = context_with_segment_and_point
 
-    result = context.segment_point_squared_distance(start, end, point)
+    result = context.segment_point_squared_distance(segment, point)
 
-    assert result == context.segment_point_squared_distance(end, start, point)
+    assert result == context.segment_point_squared_distance(
+            reverse_segment(segment), point)
+    assert result == context.segment_point_squared_distance(
+            reverse_segment_coordinates(segment),
+            reverse_point_coordinates(point))
 
 
-@given(strategies.contexts_with_rational_segments_endpoints)
-def test_self(context_with_segment_endpoints: Tuple[Context, PointsPair]
+@given(strategies.contexts_with_rational_segments)
+def test_self(context_with_segment: Tuple[Context, Segment]
               ) -> None:
-    context, segment_endpoints = context_with_segment_endpoints
-    start, end = segment_endpoints
+    context, segment = context_with_segment
 
-    assert (context.segment_point_squared_distance(start, end, start)
-            == context.segment_point_squared_distance(start, end, end)
+    assert (context.segment_point_squared_distance(segment, segment.start)
+            == context.segment_point_squared_distance(segment, segment.end)
             == 0)
