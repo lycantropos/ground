458cbf29687fcf22e030851c13f74557901d8712
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-29 11:18:14 +0300

    Complete test for 'Context.region_signed_area' property

--- tests/base_tests/context_tests/test_region_signed_area.py
+++ tests/base_tests/context_tests/test_region_signed_area.py
@@ -1,41 +1,41 @@
-from typing import (Sequence,
-                    Tuple)
+from typing import Tuple
 
 from hypothesis import given
 
 from ground.base import Context
-from ground.hints import Point
+from ground.hints import Contour
 from tests.utils import (context_to_output_coordinate_cls,
-                         reverse_sequence,
-                         rotate_sequence)
+                         reverse_contour_vertices,
+                         rotate_contour)
 from . import strategies
 
 
-@given(strategies.contexts_with_vertices)
-def test_basic(context_with_vertices: Tuple[Context, Sequence[Point]]) -> None:
-    context, vertices = context_with_vertices
+@given(strategies.contexts_with_contours)
+def test_basic(context_with_contours: Tuple[Context, Contour]) -> None:
+    context, contour = context_with_contours
 
-    result = context.region_signed_area(vertices)
+    result = context.region_signed_area(contour)
 
     assert isinstance(result, context_to_output_coordinate_cls(context))
 
 
-@given(strategies.contexts_with_rational_vertices)
-def test_reversals(context_with_vertices: Tuple[Context, Sequence[Point]]
+@given(strategies.contexts_with_rational_contours)
+def test_reversals(context_with_contours: Tuple[Context, Contour]
                    ) -> None:
-    context, vertices = context_with_vertices
+    context, contour = context_with_contours
 
-    result = context.region_signed_area(vertices)
+    result = context.region_signed_area(contour)
 
-    assert result == -context.region_signed_area(reverse_sequence(vertices))
+    assert result == -context.region_signed_area(reverse_contour_vertices(
+            contour))
 
 
-@given(strategies.contexts_with_rational_vertices, strategies.indices)
-def test_rotations(context_with_vertices: Tuple[Context, Sequence[Point]],
+@given(strategies.contexts_with_rational_contours, strategies.indices)
+def test_rotations(context_with_contours: Tuple[Context, Contour],
                    offset: int) -> None:
-    context, vertices = context_with_vertices
+    context, contour = context_with_contours
 
-    result = context.region_signed_area(vertices)
+    result = context.region_signed_area(contour)
 
     assert (result
-            == context.region_signed_area(rotate_sequence(vertices, offset)))
+            == context.region_signed_area(rotate_contour(contour, offset)))
