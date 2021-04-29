5d70e77b12f931cfb9cd82209fc03fb11dfd3947
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-29 11:16:16 +0300

    Complete test for 'Context.contour_edges' method

--- tests/base_tests/context_tests/test_contour_edges.py
+++ tests/base_tests/context_tests/test_contour_edges.py
@@ -1,33 +1,33 @@
 from collections import abc
-from typing import (Sequence,
-                    Tuple)
+from typing import Tuple
 
 from hypothesis import given
 
 from ground.base import Context
-from ground.hints import Point
+from ground.hints import Contour
 from tests.utils import (is_segment,
+                         rotate_contour,
                          rotate_sequence)
 from . import strategies
 
 
-@given(strategies.contexts_with_vertices)
-def test_basic(context_with_vertices: Tuple[Context, Sequence[Point]]) -> None:
-    context, vertices = context_with_vertices
+@given(strategies.contexts_with_contours)
+def test_basic(context_with_contour: Tuple[Context, Contour]) -> None:
+    context, contour = context_with_contour
 
-    result = context.contour_edges(vertices)
+    result = context.contour_edges(contour)
 
     assert isinstance(result, abc.Sequence)
-    assert len(result) == len(vertices)
+    assert len(result) == len(contour.vertices)
     assert all(is_segment(element) for element in result)
 
 
-@given(strategies.contexts_with_rational_vertices, strategies.indices)
-def test_rotations(context_with_vertices: Tuple[Context, Sequence[Point]],
+@given(strategies.contexts_with_rational_contours, strategies.indices)
+def test_rotations(context_with_contour: Tuple[Context, Contour],
                    offset: int) -> None:
-    context, vertices = context_with_vertices
+    context, contour = context_with_contour
 
-    result = context.contour_edges(vertices)
+    result = context.contour_edges(contour)
 
     assert (rotate_sequence(result, offset)
-            == context.contour_edges(rotate_sequence(vertices, offset)))
+            == context.contour_edges(rotate_contour(contour, offset)))
