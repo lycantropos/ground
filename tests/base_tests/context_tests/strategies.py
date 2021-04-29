858b080ad3bd4715764d207abcb99e31f25f4a8e
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-29 20:23:13 +0300

    Complete test for 'Context.segments_relation' method

--- tests/base_tests/context_tests/strategies.py
+++ tests/base_tests/context_tests/strategies.py
@@ -105,6 +105,9 @@ contexts_with_rational_segments_endpoints = (
 contexts_with_segments_endpoints = (
     contexts_with_coordinates_strategies.flatmap(to_contexts_with(
             segments_endpoints_factory)))
+contexts_with_segments_pairs = (
+    contexts_with_coordinates_strategies.flatmap(to_contexts_with(
+            compose(to_pairs, segments_factory))))
 contexts_with_rational_segments_endpoints_pairs = (
     contexts_with_rational_coordinates_strategies.flatmap(to_contexts_with(
             compose(to_pairs, segments_endpoints_factory))))
--- tests/base_tests/context_tests/test_segments_relation.py
+++ tests/base_tests/context_tests/test_segments_relation.py
@@ -4,63 +4,49 @@ from hypothesis import given
 
 from ground.base import (Context,
                          Relation)
-from tests.hints import PointsPair
+from ground.hints import Segment
 from tests.utils import (ASYMMETRIC_LINEAR_RELATIONS,
                          LINEAR_RELATIONS,
                          SYMMETRIC_LINEAR_RELATIONS,
-                         equivalence)
+                         equivalence,
+                         reverse_segment,
+                         reverse_segment_coordinates)
 from . import strategies
 
 
-@given(strategies.contexts_with_segments_endpoints_pairs)
-def test_basic(context_with_segments_endpoints_pair
-               : Tuple[Context, Tuple[PointsPair, PointsPair]]) -> None:
-    context, (first_endpoints,
-              second_endpoints) = context_with_segments_endpoints_pair
-    first_start, first_end = first_endpoints
-    second_start, second_end = second_endpoints
+@given(strategies.contexts_with_segments_pairs)
+def test_basic(context_with_segments_pair
+               : Tuple[Context, Tuple[Segment, Segment]]) -> None:
+    context, (first, second) = context_with_segments_pair
 
-    result = context.segments_relation(first_start, first_end, second_start,
-                                       second_end)
+    result = context.segments_relation(first, second)
 
     assert isinstance(result, Relation)
     assert result in LINEAR_RELATIONS
 
 
-@given(strategies.contexts_with_segments_endpoints_pairs)
-def test_endpoints_permutation(context_with_segments_endpoints_pair
-                               : Tuple[Context, Tuple[PointsPair, PointsPair]]
-                               ) -> None:
-    context, (first_endpoints,
-              second_endpoints) = context_with_segments_endpoints_pair
-    first_start, first_end = first_endpoints
-    second_start, second_end = second_endpoints
+@given(strategies.contexts_with_segments_pairs)
+def test_reversals(context_with_segments_pair
+                   : Tuple[Context, Tuple[Segment, Segment]]) -> None:
+    context, (first, second) = context_with_segments_pair
 
-    result = context.segments_relation(first_start, first_end, second_start,
-                                       second_end)
+    result = context.segments_relation(first, second)
 
-    assert (result is context.segments_relation(first_end, first_start,
-                                                second_start, second_end)
-            is context.segments_relation(first_end, first_start, second_end,
-                                         second_start)
-            is context.segments_relation(first_start, first_end, second_end,
-                                         second_start))
+    assert (result is context.segments_relation(reverse_segment(first), second)
+            is context.segments_relation(first, reverse_segment(second)))
+    assert result is context.segments_relation(
+            reverse_segment_coordinates(first),
+            reverse_segment_coordinates(second))
 
 
-@given(strategies.contexts_with_segments_endpoints_pairs)
-def test_segments_permutation(context_with_segments_endpoints_pair
-                              : Tuple[Context, Tuple[PointsPair, PointsPair]]
-                              ) -> None:
-    context, (first_endpoints,
-              second_endpoints) = context_with_segments_endpoints_pair
-    first_start, first_end = first_endpoints
-    second_start, second_end = second_endpoints
+@given(strategies.contexts_with_segments_pairs)
+def test_commutativity(context_with_segments_pair
+                       : Tuple[Context, Tuple[Segment, Segment]]) -> None:
+    context, (first, second) = context_with_segments_pair
 
-    result = context.segments_relation(first_start, first_end, second_start,
-                                       second_end)
+    result = context.segments_relation(first, second)
 
-    complement = context.segments_relation(second_start, second_end,
-                                           first_start, first_end)
+    complement = context.segments_relation(second, first)
     assert equivalence(result is complement,
                        result in SYMMETRIC_LINEAR_RELATIONS)
     assert equivalence(result is not complement,
@@ -69,12 +55,11 @@ def test_segments_permutation(context_with_segments_endpoints_pair
                        and complement in ASYMMETRIC_LINEAR_RELATIONS)
 
 
-@given(strategies.contexts_with_segments_endpoints)
-def test_self(context_with_segment_endpoints: Tuple[Context, PointsPair]
+@given(strategies.contexts_with_segments)
+def test_self(context_with_segment: Tuple[Context, Segment]
               ) -> None:
-    context, segment_endpoints = context_with_segment_endpoints
-    start, end = segment_endpoints
+    context, segment = context_with_segment
 
-    result = context.segments_relation(start, end, start, end)
+    result = context.segments_relation(segment, segment)
 
     assert result is Relation.EQUAL
