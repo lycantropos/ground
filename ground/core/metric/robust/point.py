e08ca066fa3f7bc94fbad1d571509a7109410ae1
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-28 13:18:42 +0300

    Complete 'Context.points_squared_distance' method

--- ground/core/metric/robust/point.py
+++ ground/core/metric/robust/point.py
@@ -4,4 +4,4 @@ from ground.core.robust import to_squared_points_distance
 
 
 def point_squared_distance(first: Point, second: Point) -> Scalar:
-    return to_squared_points_distance(first.x, first.y, second.x, second.y)[-1]
+    return to_squared_points_distance(first.x, first.y, second.x, second.y)
