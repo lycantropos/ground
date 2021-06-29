102e94badc7b15c5d6f1692e117bbd89b555f629
Azat Ibrakov <azatibrakov@gmail.com>
2021-06-29 23:00:28 +0300

    Complete 'Context.segment_centroid' method

--- ground/core/centroidal/exact/segment.py
+++ ground/core/centroidal/exact/segment.py
@@ -6,5 +6,7 @@ from ground.core.primitive import rationalize
 
 
 def centroid(segment: Segment, point_cls: Type[Point]) -> Point:
-    return point_cls(rationalize(segment.start.x) + rationalize(segment.end.x),
-                     rationalize(segment.start.y) + rationalize(segment.end.y))
+    return point_cls((rationalize(segment.start.x)
+                      + rationalize(segment.end.x)) / 2,
+                     (rationalize(segment.start.y)
+                      + rationalize(segment.end.y)) / 2)
