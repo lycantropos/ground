ac4d7c5e48d20f767a50615fd5612563b140cd60
Azat Ibrakov <azatibrakov@gmail.com>
2021-07-01 16:11:24 +0300

    Complete 'Context.translate_point' method

--- ground/core/translation/robust.py
+++ ground/core/translation/robust.py
@@ -10,4 +10,4 @@ def translate_point(point: Point,
                     step_x: Scalar,
                     step_y: Scalar,
                     point_cls: Type[Point]) -> Point:
-    return point_cls(Expansion(point.x, step_x), Expansion(point.y, step_y))
+    return point_cls(Expansion(point.x) + step_x, Expansion(point.y) + step_y)
