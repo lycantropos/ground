127ec02ebec6857ffaf48b2387052f58258aa7a8
Azat Ibrakov <azatibrakov@gmail.com>
2021-07-01 17:40:30 +0300

    Add test for 'Context.translate_polygon' method

--- tests/base_tests/context_tests/strategies.py
+++ tests/base_tests/context_tests/strategies.py
@@ -80,6 +80,12 @@ contexts_with_multisegments_and_scalars_pairs = (
     contexts_with_coordinates_strategies.flatmap(
             to_contexts_with(multisegments_factory, itemgetter(1),
                              itemgetter(1))))
+contexts_with_rational_polygons_and_scalars_pairs = (
+    contexts_with_rational_coordinates_strategies.flatmap(
+            to_contexts_with(polygons_factory, itemgetter(1), itemgetter(1))))
+contexts_with_polygons_and_scalars_pairs = (
+    contexts_with_coordinates_strategies.flatmap(
+            to_contexts_with(polygons_factory, itemgetter(1), itemgetter(1))))
 contexts_with_rational_points_and_scalars_pairs = (
     contexts_with_rational_coordinates_strategies.flatmap(
             to_contexts_with(points_factory, itemgetter(1), itemgetter(1))))
---
+++ tests/base_tests/context_tests/test_translate_polygon.py
@@ -0,0 +1,48 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from ground.base import Context
+from ground.hints import (Polygon,
+                          Scalar)
+from tests.utils import (reverse_polygon_border,
+                         reverse_polygon_coordinates,
+                         reverse_polygon_holes)
+from . import strategies
+
+
+@given(strategies.contexts_with_polygons_and_scalars_pairs)
+def test_basic(context_with_polygon_and_steps
+               : Tuple[Context, Polygon, Scalar, Scalar]) -> None:
+    context, polygon, step_x, step_y = context_with_polygon_and_steps
+
+    result = context.translate_polygon(polygon, step_x, step_y)
+
+    assert isinstance(result, context.polygon_cls)
+
+
+@given(strategies.contexts_with_rational_polygons_and_scalars_pairs)
+def test_round_trip(context_with_polygon_and_steps
+                    : Tuple[Context, Polygon, Scalar, Scalar]) -> None:
+    context, polygon, step_x, step_y = context_with_polygon_and_steps
+
+    result = context.translate_polygon(polygon, step_x, step_y)
+
+    assert (context.translate_polygon(result, -step_x, -step_y)
+            == context.translate_polygon(polygon, 0, 0))
+
+
+@given(strategies.contexts_with_polygons_and_scalars_pairs)
+def test_reversals(context_with_polygon_and_steps
+                   : Tuple[Context, Polygon, Scalar, Scalar]) -> None:
+    context, polygon, step_x, step_y = context_with_polygon_and_steps
+
+    result = context.translate_polygon(polygon, step_x, step_y)
+
+    assert reverse_polygon_border(result) == context.translate_polygon(
+            reverse_polygon_border(polygon), step_x, step_y)
+    assert reverse_polygon_holes(result) == context.translate_polygon(
+            reverse_polygon_holes(polygon), step_x, step_y)
+    assert (reverse_polygon_coordinates(result)
+            == context.translate_polygon(reverse_polygon_coordinates(polygon),
+                                         step_y, step_x))
