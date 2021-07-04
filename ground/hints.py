2e97ded0b4bf4ba769763f34ed65f356b265a599
Azat Ibrakov <azatibrakov@gmail.com>
2021-07-04 21:07:14 +0300

    Complete type hints

--- ground/hints.py
+++ ground/hints.py
@@ -1,22 +1,22 @@
-from .core.hints import (Box,
-                         Contour,
-                         Empty,
-                         Linear,
-                         Maybe,
-                         Mix,
-                         Multipoint,
-                         Multipolygon,
-                         Multisegment,
-                         Point,
-                         Polygon,
-                         Scalar,
-                         Segment,
-                         Shaped)
+from .core import hints
 
+Box = hints.Box
+Contour = hints.Contour
+Empty = hints.Empty
+Linear = hints.Linear
+Maybe = hints.Maybe
+Mix = hints.Mix
+Multipoint = hints.Multipoint
+Multipolygon = hints.Multipolygon
+Multisegment = hints.Multisegment
+Point = hints.Point
+Polygon = hints.Polygon
+Scalar = hints.Scalar
+Segment = hints.Segment
+Shaped = hints.Shaped
 Box.__module__ = __name__
 Contour.__module__ = __name__
 Empty.__module__ = __name__
-Linear.__module__ = __name__
 Maybe.__module__ = __name__
 Mix.__module__ = __name__
 Multipoint.__module__ = __name__
@@ -24,6 +24,4 @@ Multipolygon.__module__ = __name__
 Multisegment.__module__ = __name__
 Point.__module__ = __name__
 Polygon.__module__ = __name__
-Scalar.__module__ = __name__
 Segment.__module__ = __name__
-Shaped.__module__ = __name__
