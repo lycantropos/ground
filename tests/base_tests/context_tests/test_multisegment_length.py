5c52499aabc75d64b85982fd01582bb961f72665
Azat Ibrakov <azatibrakov@gmail.com>
2021-07-05 13:50:25 +0300

    Add test for 'Context.multisegment_length' method

---
+++ tests/base_tests/context_tests/test_multisegment_length.py
@@ -0,0 +1,44 @@
+from typing import Tuple
+
+from hypothesis import given
+from symba.base import Expression
+
+from ground.base import Context
+from ground.hints import Multisegment
+from tests.utils import (reverse_multisegment,
+                         reverse_multisegment_coordinates)
+from . import strategies
+
+
+@given(strategies.contexts_with_multisegments)
+def test_basic(context_with_multisegment: Tuple[Context, Multisegment]
+               ) -> None:
+    context, multisegment = context_with_multisegment
+
+    result = context.multisegment_length(multisegment)
+
+    assert isinstance(result, Expression)
+
+
+@given(strategies.contexts_with_multisegments)
+def test_value(context_with_multisegment: Tuple[Context, Multisegment]
+               ) -> None:
+    context, multisegment = context_with_multisegment
+
+    result = context.multisegment_length(multisegment)
+
+    assert all(result > context.segment_length(segment)
+               for segment in multisegment.segments)
+
+
+@given(strategies.contexts_with_rational_multisegments)
+def test_reversals(context_with_multisegment: Tuple[Context, Multisegment]
+                   ) -> None:
+    context, multisegment = context_with_multisegment
+
+    result = context.multisegment_length(multisegment)
+
+    assert result == context.multisegment_length(
+            reverse_multisegment(multisegment))
+    assert result == context.multisegment_length(
+            reverse_multisegment_coordinates(multisegment))
