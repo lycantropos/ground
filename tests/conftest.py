93e2bdaeea82f3d5fc10d5b770424269218e4d41
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-16 00:42:12 +0700

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from hypothesis import HealthCheck, settings
 
 is_pypy = platform.python_implementation() == 'PyPy'
 on_ci = bool(os.getenv('CI', False))
-max_examples = ((-(-settings.default.max_examples // 25)
+max_examples = ((-(-settings.default.max_examples // 50)
                  if is_pypy
                  else -(-settings.default.max_examples // 5))
                 if on_ci
