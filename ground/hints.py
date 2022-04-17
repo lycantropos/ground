8e63e3d27c5da5aa6f9c31580ca747b4baa6c994
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-17 12:03:43 +0700

    Complete imports

--- ground/hints.py
+++ ground/hints.py
@@ -1,19 +1,19 @@
-from .core import hints
+from .core import hints as _hints
 
-Box = hints.Box
-Contour = hints.Contour
-Empty = hints.Empty
-Linear = hints.Linear
-Maybe = hints.Maybe
-Mix = hints.Mix
-Multipoint = hints.Multipoint
-Multipolygon = hints.Multipolygon
-Multisegment = hints.Multisegment
-Point = hints.Point
-Polygon = hints.Polygon
-Scalar = hints.Scalar
-Segment = hints.Segment
-Shaped = hints.Shaped
+Box = _hints.Box
+Contour = _hints.Contour
+Empty = _hints.Empty
+Linear = _hints.Linear
+Maybe = _hints.Maybe
+Mix = _hints.Mix
+Multipoint = _hints.Multipoint
+Multipolygon = _hints.Multipolygon
+Multisegment = _hints.Multisegment
+Point = _hints.Point
+Polygon = _hints.Polygon
+Scalar = _hints.Scalar
+Segment = _hints.Segment
+Shaped = _hints.Shaped
 Box.__module__ = __name__
 Contour.__module__ = __name__
 Empty.__module__ = __name__
