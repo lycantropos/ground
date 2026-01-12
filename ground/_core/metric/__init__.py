d0d7708445b0327941cc72f4f286e343e1679cf5
Azat Ibrakov <azatibrakov@gmail.com>
2026-01-12 23:27:12 +0100

    Fix imports

--- ground/_core/metric/__init__.py
+++ ground/_core/metric/__init__.py
@@ -1,1 +1,5 @@
-from .context import Context as Context, plain_context as plain_context
+from .context import (
+    Context as Context,
+    PointPointMetric as PointPointMetric,
+    plain_context as plain_context,
+)
